4ee5793bfd0b743fb6a268100888f9b614fad0a9
= <=>
2016-09-28 13:38:17 +0900

    open new tab

--- background.js
+++ background.js
@@ -1,3 +1,10 @@
 chrome.browserAction.onClicked.addListener(function(tab){
-	chrome.extension.getBackgroundPage().console.log('clicked'); 
+	// chrome.extension.getBackgroundPage().console.log('clicked');
+    loadHtml("setting.html");
 });
+
+function loadHtml(fileName){
+    chrome.tabs.getAllInWindow(null, function(windows){
+        chrome.tabs.create({ url: fileName });
+    });
+}
